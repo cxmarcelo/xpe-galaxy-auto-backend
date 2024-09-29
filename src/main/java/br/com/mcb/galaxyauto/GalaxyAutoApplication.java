9badc8807b82c533ceedea565137f3becb075e77
Marcelo C <marcelo.c.b22@gmail.com>
2024-09-29 18:55:32 -0300

    Projeto Criado

---
+++ src/main/java/br/com/mcb/galaxyauto/GalaxyAutoApplication.java
@@ -0,0 +1,13 @@
+package br.com.mcb.galaxyauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyAutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyAutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mcb/galaxyauto/GalaxyAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mcb.galaxyauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
